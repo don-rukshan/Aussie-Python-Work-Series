68a61e525ea96520aaa1d4db02e2e45b1bc9bfa2
don-rukshan <md.rukshan@gmail.com>
2020-03-03 19:31:44 +0530

    Complete Series

---
+++ Workshop 4/Bash Script/wordhisto.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+python countWords.py
+python histogram.py
---
+++ Workshop 4/Task 2/public_htmls/code/histogram.py
@@ -0,0 +1,21 @@
+def n0_punch(f):
+    punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+    no_punct = ""
+    for char in f:
+        if char not in punctuations:
+            no_punct = no_punct + char
+    return no_punct
+
+f = (input("Enter a String: ")).upper()
+w = n0_punch(f)
+
+x = list(w.split())
+
+wordcount={}
+for word in x:
+    if word not in wordcount:
+        wordcount[word] = 1
+    else:
+        wordcount[word] += 1
+for k,v in wordcount.items():
+        print(k,"[",'*'*v,"]", round((v / len(x))*100),"%")
